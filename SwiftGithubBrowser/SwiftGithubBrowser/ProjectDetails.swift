3daec7c8d1f1ab34fc67cec4c077276741bfd3cc
Calo, Ignazio <icalo@ebay.com>
2017-05-02 23:30:47 +0200

    Add stub model classes

---
+++ SwiftGithubBrowser/SwiftGithubBrowser/ProjectDetails.swift
@@ -0,0 +1,12 @@
+//
+//  ProjectDetails.swift
+//  SwiftGithubBrowser
+//
+//  Created by Calo, Ignazio on 02/05/2017.
+//
+//
+
+import Foundation
+struct ProjectDetails {
+    
+}
---
+++ SwiftGithubBrowser/SwiftGithubBrowser/ProjectSummary.swift
@@ -0,0 +1,13 @@
+//
+//  ProjectSummary.swift
+//  SwiftGithubBrowser
+//
+//  Created by Calo, Ignazio on 02/05/2017.
+//
+//
+
+import Foundation
+
+struct ProjectEnum {
+
+}

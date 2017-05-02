58252b09c07235787715c8583fa6f0de851216e4
Calo, Ignazio <icalo@ebay.com>
2017-05-02 23:04:01 +0200

    Create black project template

---
+++ SwiftGithubBrowser/SwiftGithubBrowser/ProjectsListViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftGithubBrowser
+//
+//  Created by Calo, Ignazio on 02/05/2017.
+//
+//
+
+import UIKit
+
+class ProjectsListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
